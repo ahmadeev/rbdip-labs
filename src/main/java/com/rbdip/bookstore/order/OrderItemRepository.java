2ff45d1b7cf70b1a9df32f323f50c74e19168a4d
danis <ahrnadeev@gmail.com>
2026-09-19 23:41:29 +0300

    refactor(lab-4): fix N+1

--- src/main/java/com/rbdip/bookstore/order/OrderItemRepository.java
+++ src/main/java/com/rbdip/bookstore/order/OrderItemRepository.java
@@ -1,11 +1,14 @@
 package com.rbdip.bookstore.order;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     // Намеренно нет @EntityGraph/JOIN FETCH-варианта - используется в цикле
     // контроллером, что и создаёт N+1 (см. OrderController#listOrders).
+    @EntityGraph(attributePaths = "product")
     List<OrderItem> findByOrderId(Long orderId);
 }
